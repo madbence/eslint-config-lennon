03a96693f217686508957870c853c1a173681b4d
Bence Dányi <bence@danyi.me>
2017-09-23 16:08:01 +0200

    feat: relax curly rule

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ module.exports = {
     'class-methods-use-this': 'warn',
     'complexity': 'off',
     'consistent-return': 'error',
-    'curly': ['error', 'multi'],
+    'curly': ['error', 'multi-line'],
     'default-case': 'warn',
     'dot-location': ['error', 'property'],
     'dot-notation': 'off',
