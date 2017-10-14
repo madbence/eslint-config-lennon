37ce3acae56baca3c9e253efcc6efe07a05ae95f
Bence Dányi <bence@danyi.me>
2017-10-14 17:02:01 +0200

    feat: add react rules

---
+++ react.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    "react",
+  ],
+  rules: {
+    'react/boolean-prop-naming': "off",
+    'react/default-props-match-prop-types': 'error'
+    'react/display-name': 'off',
+    'react/forbid-component-props': 'off',
+    'react/forbid-elements': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/no-array-index-key': 'off',
+
+    'react/react-in-jsx-scope': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+};
